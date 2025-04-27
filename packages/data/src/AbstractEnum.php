15bdcaf4a8be6ee6f702a893daa8d6ccb553dfef
Darwin Luague <darwinluague9001@gmail.com>
2025-04-27 16:54:18 +0800

    Add AbstractEnum base class for enum value handling
    This class provides a foundation for working with enums, including conversion methods to and from various types such as strings, integers, arrays, and Eloquent models. It ensures consistent handling of nullable and non-nullable enums across the application.

---
+++ packages/data/src/AbstractEnum.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Metatech\Support\Abstracts;
+
+use BackedEnum;
+use Illuminate\Database\Eloquent\Model;
+use Mom\Data\AbstractValue;
+
+abstract class AbstractEnum extends AbstractValue
+{
+    abstract public function toNullableEnum(): ?BackedEnum;
+
+    abstract public function toEnum(): BackedEnum;
+
+    public static function fromEloquentModel(Model $model): static
+    {
+        $value = $model->getAttributes()[static::getDatabaseTableColumnName()] ?? null;
+
+        return new static(value: $value);
+    }
+
+    public static function fromArray(array $item): static
+    {
+        return new static(value: $item[static::getName()] ?? null);
+    }
+
+    public static function fromString(string $value): static
+    {
+        return new static($value);
+    }
+
+    public static function fromInteger(int $value): static
+    {
+        return new static($value);
+    }
+
+    public static function fromNullableString(?string $value): static
+    {
+        return new static($value);
+    }
+
+    public static function fromEnum(BackedEnum $value): static
+    {
+        return new static($value);
+    }
+
+    public static function fromNullableEnum(?BackedEnum $value): static
+    {
+        return new static($value);
+    }
+
+    public function toNullableString(): ?string
+    {
+        return $this->toNullableEnum()?->value;
+    }
+
+    public function toString(): string
+    {
+        return $this->toEnum()->value;
+    }
+
+    public function toPrimitive(): ?string
+    {
+        return $this->toNullableString();
+    }
+
+    public function isNull(): bool
+    {
+        return null === $this->toNullableString();
+    }
+}
